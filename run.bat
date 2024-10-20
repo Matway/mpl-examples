fe6d91383b2e750ca6a9428a175d03da21f508f5
y <x>
2024-10-20 00:27:59 +0000

    Ditto

--- run.bat
+++ run.bat
@@ -6,19 +6,16 @@ mkdir output
 echo Compilation. Please wait...
 echo C++
 mplc .\MPL\common.mpl -ndebug -begin_func startUp -end_func tearDown -o output\common.ll -I .\sl -I .\MPL -I .\MPL\windows
-clang .\bubbleSort\bubbleSortCpp\test.cpp         .\output\common.ll -o output\bubbleSortCpp.exe     -I .\Cpp -Wno-override-module -D NDEBUG -O3 -march=native -Wall --std=c++20
 clang .\fibonacciCycle\fibonacciCycleCpp\test.cpp .\output\common.ll -o output\fibonacciCycleCpp.exe -I .\Cpp -Wno-override-module -D NDEBUG -O3 -march=native -Wall --std=c++20
 clang .\fibonacciRec\fibonacciRecCpp\test.cpp     .\output\common.ll -o output\fibonacciRecCpp.exe   -I .\Cpp -Wno-override-module -D NDEBUG -O3 -march=native -Wall --std=c++20
 clang .\ip\ipCpp\test.cpp                         .\output\common.ll -o output\ipCpp.exe             -I .\Cpp -Wno-override-module -D NDEBUG -O3 -march=native -Wall --std=c++20
 clang .\table\tableCpp\test.cpp                   .\output\common.ll -o output\tableCpp.exe          -I .\Cpp -Wno-override-module -D NDEBUG -O3 -march=native -Wall --std=c++20
 echo mplc
-mplc .\bubbleSort\bubbleSortMpl\test.mpl         -I .\sl -I .\MPL -I .\MPL\windows -o output\bubbleSortMpl.ll     -ndebug
 mplc .\fibonacciCycle\fibonacciCycleMpl\test.mpl -I .\sl -I .\MPL -I .\MPL\windows -o output\fibonacciCycleMpl.ll -ndebug
 mplc .\fibonacciRec\fibonacciRecMpl\test.mpl     -I .\sl -I .\MPL -I .\MPL\windows -o output\fibonacciRecMpl.ll   -ndebug
 mplc .\ip\ipMpl\test.mpl                         -I .\sl -I .\MPL -I .\MPL\windows -o output\ipMpl.ll             -ndebug
 mplc .\table\tableMpl\test.mpl                   -I .\sl -I .\MPL -I .\MPL\windows -o output\tableMpl.ll          -ndebug
 echo clang\link
-clang .\output\bubbleSortMpl.ll     -o output\bubbleSortMpl.exe     -Wno-override-module -O3 -march=native
 clang .\output\fibonacciCycleMpl.ll -o output\fibonacciCycleMpl.exe -Wno-override-module -O3 -march=native
 clang .\output\fibonacciRecMpl.ll   -o output\fibonacciRecMpl.exe   -Wno-override-module -O3 -march=native
 clang .\output\ipMpl.ll             -o output\ipMpl.exe             -Wno-override-module -O3 -march=native
@@ -28,21 +25,18 @@ echo:
 echo Testing. Please wait...
 echo:
 echo MPL:
-.\output\bubbleSortMpl.exe     >NUL
 .\output\fibonacciCycleMpl.exe >NUL
 .\output\fibonacciRecMpl.exe   >NUL
 .\output\ipMpl.exe             >NUL
 .\output\tableMpl.exe          >NUL
 echo:
 echo C++:
-.\output\bubbleSortCpp.exe     >NUL
 .\output\fibonacciCycleCpp.exe >NUL
 .\output\fibonacciRecCpp.exe   >NUL
 .\output\ipCpp.exe             >NUL
 .\output\tableCpp.exe          >NUL
 echo:
 echo Python:
-echo | set /p="bubbleSort	"     & call timecmd "python .\bubbleSort\bubbleSortPython\test.py         -OO >NUL"
 echo | set /p="fibonacciCycle	" & call timecmd "python .\fibonacciCycle\fibonacciCyclePython\test.py -OO >NUL"
 echo | set /p="fibonacciRec	"   & call timecmd "python .\fibonacciRec\fibonacciRecPython\test.py     -OO >NUL"
 echo | set /p="ip		"           & call timecmd "python .\ip\ipPython\test.py                         -OO >NUL"
--- run.sh
+++ run.sh
@@ -6,19 +6,16 @@ mkdir output
 echo Building. Please wait...
 echo C++
 mplc ./MPL/common.mpl -ndebug -begin_func startUp -end_func tearDown -o output/common.ll -I ./sl -I ./MPL -I ./MPL/linux
-clang++ ./bubbleSort/bubbleSortCpp/test.cpp         ./output/common.ll -o output/bubbleSortCpp     -I ./Cpp -Wno-override-module -D NDEBUG -O3 -march=native -Wall --std=c++20
 clang++ ./fibonacciCycle/fibonacciCycleCpp/test.cpp ./output/common.ll -o output/fibonacciCycleCpp -I ./Cpp -Wno-override-module -D NDEBUG -O3 -march=native -Wall --std=c++20
 clang++ ./fibonacciRec/fibonacciRecCpp/test.cpp     ./output/common.ll -o output/fibonacciRecCpp   -I ./Cpp -Wno-override-module -D NDEBUG -O3 -march=native -Wall --std=c++20
 clang++ ./ip/ipCpp/test.cpp                         ./output/common.ll -o output/ipCpp             -I ./Cpp -Wno-override-module -D NDEBUG -O3 -march=native -Wall --std=c++20
 clang++ ./table/tableCpp/test.cpp                   ./output/common.ll -o output/tableCpp          -I ./Cpp -Wno-override-module -D NDEBUG -O3 -march=native -Wall --std=c++20
 echo mplc
-mplc ./bubbleSort/bubbleSortMpl/test.mpl         -I ./sl -I ./MPL -I ./MPL/linux -o output/bubbleSortMpl.ll     -ndebug
 mplc ./fibonacciCycle/fibonacciCycleMpl/test.mpl -I ./sl -I ./MPL -I ./MPL/linux -o output/fibonacciCycleMpl.ll -ndebug
 mplc ./fibonacciRec/fibonacciRecMpl/test.mpl     -I ./sl -I ./MPL -I ./MPL/linux -o output/fibonacciRecMpl.ll   -ndebug
 mplc ./ip/ipMpl/test.mpl                         -I ./sl -I ./MPL -I ./MPL/linux -o output/ipMpl.ll             -ndebug
 mplc ./table/tableMpl/test.mpl                   -I ./sl -I ./MPL -I ./MPL/linux -o output/tableMpl.ll          -ndebug
 echo clang++
-clang++ ./output/bubbleSortMpl.ll     -o output/bubbleSortMpl     -Wno-override-module -O3 -march=native
 clang++ ./output/fibonacciCycleMpl.ll -o output/fibonacciCycleMpl -Wno-override-module -O3 -march=native
 clang++ ./output/fibonacciRecMpl.ll   -o output/fibonacciRecMpl   -Wno-override-module -O3 -march=native
 clang++ ./output/ipMpl.ll             -o output/ipMpl             -Wno-override-module -O3 -march=native
@@ -28,21 +25,18 @@ echo
 echo Testing. Please wait...
 echo
 echo MPL:
-./output/bubbleSortMpl     > /dev/null
 ./output/fibonacciCycleMpl > /dev/null
 ./output/fibonacciRecMpl   > /dev/null
 ./output/ipMpl             > /dev/null
 ./output/tableMpl          > /dev/null
 echo
 echo C++:
-./output/bubbleSortCpp     > /dev/null
 ./output/fibonacciCycleCpp > /dev/null
 ./output/fibonacciRecCpp   > /dev/null
 ./output/ipCpp             > /dev/null
 ./output/tableCpp          > /dev/null
 echo
 echo Python:
-echo bubbleSort     & time python3 ./bubbleSort/bubbleSortPython/test.py         -OO > /dev/null
 echo fibonacciCycle & time python3 ./fibonacciCycle/fibonacciCyclePython/test.py -OO > /dev/null
 echo fibonacciRec   & time python3 ./fibonacciRec/fibonacciRecPython/test.py     -OO > /dev/null
 echo ip             & time python3 ./ip/ipPython/test.py                         -OO > /dev/null
